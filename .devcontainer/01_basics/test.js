c3b35fe18b7d922b77d1db7dc5834d77c4d4b8c9
nabinsth36 <132727360+nabinsth36@users.noreply.github.com>
2024-02-02 08:28:23 +0000

    function is added

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,1 +1,12 @@
-console.log('nabin shrestha')
+function add(arr){
+    if (arr.length === 0) return "Array is empty."
+
+    let sum = 0;
+    for (let i = 0; i<arr.legnth; i++){
+        sum+=arr.length[i]
+    }
+    return sum;
+}
+
+let numbers = [5, 2, 3, 9]
+console.log(add(numbers));
