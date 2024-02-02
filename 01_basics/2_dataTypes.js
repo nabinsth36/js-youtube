11c67a0c168ddfa160c525016c6a15eafe9c3399
nabinsth36 <132727360+nabinsth36@users.noreply.github.com>
2024-02-02 17:43:03 +0000

    data types

---
+++ 01_basics/2_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+// alert(3 + 3);
+
+let isLoggedIn = false;
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true / false
+// null => standalone value
+// undefined =>
+
+// object
+let age  = 25;
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+const accountId = 144553
+let accountEmail = "hitesh@gmail.com"
+console.log(accountId);
+console.log(accountEmail);
+accountEmail = "hc@hc.com"
+var accountPw = "121212"
+accountPw = "22222";
+let accountCity = "Bharatpur";
+let accountState;
+
+console.log(accountPw);
+console.table([accountId, accountEmail, accountPw, accountCity, accountState])
