7578c52733fbbf21aa1af6073ed166805ded0a4c
nabinsth36 <132727360+nabinsth36@users.noreply.github.com>
2024-02-02 17:28:19 +0545

    updated test js

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -10,3 +10,29 @@ function add(arr){
 
 let numbers = [5, 2, 3, 9]
 console.log(add(numbers));
+
+
+// Creating an array of strings
+
+let myArray = ['apple', 'banana', 'orange', 'grape'];
+
+// Accessing elements of the array using indices
+
+console.log("Elementa at indesx 0:", myArray[0]);
+console.log["Elements at index 2:", myArray[2]];
+
+// Modifying elements of the array
+myArray[0] = 'kiwi';
+console.log('Modified array:', myArray);
+
+// Adding elements to the array
+myArray.push("pineapple");
+console.log("Array after adding pineapple", myArray);
+
+// Removing elements from the array
+let removedElement = myArray.pop();
+console.log("Array after removing elements:", myArray);
+console.log("Removed element:", removedElement);
+
+// Length of the array
+console.log("Length of the array:", myArray.length);
