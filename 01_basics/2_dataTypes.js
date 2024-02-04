d86af994993f63cc866cdf171fb974d975aa9b8d
nabinsth36 <132727360+nabinsth36@users.noreply.github.com>
2024-02-04 08:33:05 +0000

    tempconv created and devcontainer updated

--- 01_basics/2_dataTypes.js
+++ 01_basics/2_dataTypes.js
@@ -17,3 +17,63 @@ console.log(typeof age);
 console.log(typeof null);
 console.log(typeof undefined);
 
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// Operations
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+let str1 = 'Hello';
+let str2 = 'nabin';
+console.log(str1 + str2);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2;
+console.log(num1);
+console.log("");
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+// Postfix increment
+let x = 3;
+const y = x++
+console.log(`x is ${x}, y is ${y}`);
+
+let x2 = 3n;
+const y2 = x2++;
+console.log(`x2 is ${x2}, y2 is ${y2}`);
+
+// Prefix increment
+let a = 3;
+const b = ++a;
+console.log(`a is ${a}, b is ${b}`)
+
+
+
+// Stack (Primitive), Heap (Non-primitive)
+
+let myYoutubename = 'hiteshdotcom';
+let anothername = myYoutubename;
+anothername = 'chaiaurcode';
+console.log(anothername);
+console.log(myYoutubename);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl",
+}
+
+let userTwo = userOne
+
+userTwo.email ="hitesh@google.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
