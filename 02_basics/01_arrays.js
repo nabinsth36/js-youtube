93eeabc3c31d22d9a2e44656a800d5015b45abef
nabinsth36 <132727360+nabinsth36@users.noreply.github.com>
2024-02-06 02:57:37 +0000

    array update

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -38,3 +38,24 @@ console.log("B ", myArray);
 const myn2 = myArray.splice(1,3) 
 console.log("C ", myArray);
 console.log(myn2);
+
+const fruits =[];
+fruits.push('banana','apple','orange');
+console.log(fruits.length);
+
+fruits[5] = 'mango';
+console.log(fruits[5]);
+console.log(fruits);
+console.log(Object.keys(fruits));
+console.log(fruits.length);
+
+fruits.length = 10;
+console.log(fruits);
+console.log(Object.keys(fruits));
+console.log(fruits.length);
+console.log(fruits[8]);
+
+fruits.length = 2
+console.log(Object.keys(fruits));
+console.log(fruits.length);
+console.log(fruits);
