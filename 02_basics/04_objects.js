4f4a3274bd859a21ec8034b4427da0954dad65cf
nabinsth36 <132727360+nabinsth36@users.noreply.github.com>
2024-02-07 02:33:44 +0000

    objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -17,7 +17,7 @@ const regularUser = {
   },
 };
 
-console.log(regularUser.fullname?.userfullname.firstname);
+// console.log(regularUser.fullname?.userfullname.firstname);
 const obj1 = { 1: "a", 2: "b" };
 const obj2 = { 3: "a", 4: "b" };
 
@@ -43,10 +43,43 @@ const users = [
 ];
 
 users[1].email;
-console.log(tinderUser);
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
 
-console.log(Object.entries(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+// console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+
+
+const course = {
+  coursename: "js in hindi",
+  price: '999',
+  courseInstructor: 'hitesh'
+}
+
+const {courseInstructor: instructor} = course;
+
+// console.log(courseInstructor);
+console.log(instructor);
+/* 
+const navbar = (company) => {}
+
+navbar(company = "hitesh")
+console.log(navbar); 
+*/
+
+/* {
+  "name": "hitesh",
+  "coursename": "js in hindi",
+  "price": "free"
+
+} */
+
+[
+  {},
+  {
+  
+  },
+  {}
+]
