4e590f0f60a67f8b72d4a63552c787299b51ab3d
nabinsth36 <132727360+nabinsth36@users.noreply.github.com>
2024-02-10 16:16:32 +0000

    for loop learn

--- 05_iteratirons/one.js
+++ 05_iteratirons/one.js
@@ -1,5 +1,5 @@
 // for
-
+/* 
 for (let index = 0; index < 10; index++) {
     const element = index;
     // console.log(element);
@@ -7,12 +7,28 @@ for (let index = 0; index < 10; index++) {
         console.log("5 is best number");
     }
     console.log(element);
-}
-
+} */
+/* 
 for (let i = 0; i <=10; i++) {
     console.log(`Outer loop value: ${i}`);
     for (let j = 0; j <=10; j++) {
-        console.log(`Inner loop value ${j} and inner loop ${i}`);
+        // console.log(`Inner loop value ${j} and inner loop ${i}`);
+        console.log(`${i} * ${j} = ${i*j}`);
     }
     
+} */
+/* 
+let myArray = ['flash', 'batman', 'superman']
+
+for (let i = 0; i<myArray.length; i++) {
+    const element = myArray[i];
+    console.log(element);
+} */
+
+for (let i = 1; i<=20; i++) {
+    if(i == 5){
+        console.log(`Detected 5`);
+        continue
+    }
+    console.log(`value of i is ${i}`);
 }
