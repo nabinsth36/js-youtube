ac15ae8d0cf52893f83293f73de93aba603c4903
Nabin Shrestha <nabinsth@gmail.com>
2024-05-31 20:40:36 +0545

    js revision

---
+++ js_revision/datatypes.js
@@ -0,0 +1,7 @@
+"use strict";
+// alert(3 + 3);
+console.log(3 + 3);
+
+let name = "hitesh";
+let age = 18;
+let isLoggedIn = true;
---
+++ js_revision/seven.js
@@ -0,0 +1,10 @@
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const newNums = myNumbers.map((num) => num + 10);
+
+const newNums = [];
+myNumbers.forEach((num) => {
+  return newNums.push(num + 10);
+});
+
+console.log(newNums);
