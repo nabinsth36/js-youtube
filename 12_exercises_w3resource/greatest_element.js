fe1de1d85a49b9dd72ac88b33e4cee4995b5c5f1
Nabin Shrestha <nabinsth@gmail.com>
2024-06-12 23:17:59 +0545

    remove greatest_element

--- 12_exercises_w3resource/greatest_element.js
+++
@@ -1,9 +0,0 @@
-// 90. Write a JavaScript program to find the kth greatest element in a given array of integers.
-
-function greatestElement(num) {
-  let arr = num.sort((a, b) => b - a);
-  return `Greatest element in a given array of integers = ${arr[0]}`;
-}
-
-let num = [5, 2, 8, 12, 3];
-console.log(greatestElement(num));
